ce19f9dddaf6ccf9e40c183d6de22e14db02f004
diki449 <76251649+diki449@users.noreply.github.com>
2020-12-19 20:35:20 +0800

    Add files via upload

---
+++ src/matakuliah/MataKuliah.java
@@ -0,0 +1,16 @@
+package matakuliah;
+
+/**
+ *
+ * @author user
+ */
+public class MataKuliah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FormMataKuliah().show();
+    }
+    
+}
